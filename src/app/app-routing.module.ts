13bcb589aa208579f156d85b7b8426e1a8673841
A Shendage <shendageanand7@gmail.com>
2024-01-20 00:09:50 +0530

    Added my project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './modules/landing-page/landing-page.component';
+import { AboutPageComponent } from './modules/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : LandingPageComponent
+  },
+  {
+    path : 'about',
+    component : AboutPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './modules/landing-page/landing-page.component';
+import { AboutPageComponent } from './modules/about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
