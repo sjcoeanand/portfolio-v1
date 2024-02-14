c214ad3774a975bbdf1236bdae0ba08d39ba895b
A Shendage <shendageanand7@gmail.com>
2024-02-14 20:26:08 +0530

    first palindrome string, snake game update

--- src/assets/jsfiles/dsa-palindrome.js
+++ src/assets/jsfiles/dsa-palindrome.js
@@ -5,3 +5,19 @@ const isPalindrome = function(item){
 let res = isPalindrome(121);
 console.log(`isPalindrome = ${res}`);
 // -------------------------------
+//find first palindrome string from array of strings
+let firstPalindrome = function(words1) {
+   console.log(words1.find(isPalindrome))
+};
+function isPalindrome(singleString){
+let a;    
+ if(singleString === singleString.split('').reverse().join('')) {
+   a = singleString
+ } else {
+  a = ""
+ }
+ return a
+}
+firstPalindrome(["abc","car","ada","racecar","cool"])
+firstPalindrome(["notapalindrome","racecar"])
+firstPalindrome(["def","ghi"])
